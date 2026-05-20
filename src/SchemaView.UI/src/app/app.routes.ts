af088eb7797ecf6eba68ec8c5d442b05dbc18e4b
Muhammed Minhaj VP <vpmuhammedminhaj02@gmail.com>
2026-05-20 12:46:43 +0530

    migrate to lazy load

--- src/SchemaView.UI/src/app/app.routes.ts
+++ src/SchemaView.UI/src/app/app.routes.ts
@@ -1,10 +1,25 @@
 import { Routes } from '@angular/router';
-import { InitialSetupPage } from './features/setup/pages/initial-setup-page/initial-setup-page';
-import { ConnectionsListPage } from './features/setup/pages/connections-list-page/connections-list-page';
-import { DatabaseExplorerPage } from './features/setup/pages/database-explorer-page/database-explorer-page';
 
 export const routes: Routes = [
-  { path: '', component: InitialSetupPage },
-  { path: 'connections', component: ConnectionsListPage },
-  { path: 'connections/:id', component: DatabaseExplorerPage },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/setup/pages/initial-setup-page/initial-setup-page').then(
+        (m) => m.InitialSetupPage,
+      ),
+  },
+  {
+    path: 'connections',
+    loadComponent: () =>
+      import('./features/setup/pages/connections-list-page/connections-list-page').then(
+        (m) => m.ConnectionsListPage,
+      ),
+  },
+  {
+    path: 'connections/:id',
+    loadComponent: () =>
+      import('./features/setup/pages/database-explorer-page/database-explorer-page').then(
+        (m) => m.DatabaseExplorerPage,
+      ),
+  },
 ];
